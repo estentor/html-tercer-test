19dbeb7f556094e1a2c2504183171e0e4131cdbc
lmsoto <luis.miguel.soto@monsanto.com>
2018-09-12 16:42:29 -0300

    cambios al footer

--- js/custom.js
+++ js/custom.js
@@ -1,8 +1,16 @@
-$(window).scroll(function() {    
-    var scroll = $(window).scrollTop();
-    if (scroll >= 500) {
-        $(".logo").removeClass("logo-hidden");
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        document.getElementById("myBtn").style.display = "block";
     } else {
-        $(".logo").addClass("logo-hidden");
+        document.getElementById("myBtn").style.display = "none";
     }
-});
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+    document.body.scrollTop = 0; // For Safari
+    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
